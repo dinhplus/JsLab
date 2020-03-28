8752844f80444e29de51476f0000f631959b609b
dinhplus <dinhdx.hust@gmail.com>
2020-03-28 10:55:18 +0700

    complete Edit and Delete Feature

--- NodeJsProject/index.js
+++ NodeJsProject/index.js
@@ -36,18 +36,26 @@ function showMenu() {
             searchContact();
             main();
             break;
-            // case "3":
-            //     showListContact();
-            //     editContact();
-            //     saveAll();
-            //     break;
-            // case "4":
-            //     showListContact();
-            //     deleteContact();
-            //     saveAll();
-            //     break;
-            // default:
-            //     break;
+        case "3":
+            console.log('chọn số tương ứng với contact để chỉnh sửa')
+            showListContact();
+            var selectToEdit = readlineSync.question('>>');
+            editContact(selectToEdit - 1);
+            saveAll();
+            if (saveAll()) console.log('saved Done!');
+            main();
+            break;
+        case "4":
+            console.log('chọn số tương ứng với contact để chỉnh Xóa')
+            showListContact();
+            var selectToDelete = readlineSync.question('>>')
+            deleteContact(selectToDelete - 1);
+            saveAll();
+            console.log('Đã xóa!');
+            main()
+            break;
+        default:
+            break;
     }
 }
 
@@ -63,13 +71,14 @@ function showCreateContact() {
  *  - phân giải chuỗi thành dạng mảng
  *  - so sánh từng phần tử mảng value[] với mảng name[],
  *  - Nếu trùng khớp toàn bộ giá trị của value với name thì getResult(),
- *  - nếu lệch một kí tự thì chuyển qua object khác
- *- cũng làm tương tự như với name;
- *
+ *  - nếu lệch một kí tự thì chuyển qua value[key] khác
+ *- cũng làm tương tự như với phoneNumber;
+ *- vì để tiết kiệm time thì nối luôn cả name và number lại để so sánh cho dễ
  */
 function searchContact() {
     var result = [];
     let checkList = [];
+
     let value = readlineSync.question("contact Name or phone Number: ");
 
     for (const key in contacts) {
@@ -104,6 +113,31 @@ function getResult(index) {
     return result;
 }
 
+function showListContact() {
+    for (let index = 0; index < contacts.length; index++) {
+        console.log((index + 1) + '. ' + (contacts[index].name + ':' + contacts[index].phoneNumber));
+    }
+}
+
+function editContact(key) {
+    console.log('The old name is:', contacts[key]['name']);
+    let newName = readlineSync.question('New name :');
+    console.log('The old phone Number is:', contacts[key]['phoneNumber']);
+    let newPhoneNumber = readlineSync.question('New Phone Number :');
+    contacts[key] = { name: newName, phoneNumber: newPhoneNumber };
+    return contacts;
+}
+
+function deleteContact(key) {
+    console.log('Do you really want to delete this contact?' + '\n' + contacts[key]['name'] + ':' + contacts[key]['phoneNumber']);
+    console.log('enter yes to delete, any letter to Back menu');
+    let validToDelete = readlineSync.question("(yes/no):");
+    if (validToDelete === 'yes') {
+        contacts.splice(key, 1);
+    } else main();
+    return contacts;
+}
+
 function main() {
     loadData();
     showMenu();
@@ -118,18 +152,26 @@ function main() {
             searchContact();
             main();
             break;
-            // case "3":
-            //     showListContact();
-            //     editContact();
-            //     saveAll();
-            //     break;
-            // case "4":
-            //     showListContact();
-            //     deleteContact();
-            //     saveAll();
-            //     break;
-            // default:
-            //     break;
+        case "3":
+            console.log('chọn số tương ứng với contact để chỉnh sửa')
+            showListContact();
+            var selectToEdit = readlineSync.question('>>');
+            editContact(selectToEdit - 1);
+            saveAll();
+            console.log('saved Done!');
+            main();
+            break;
+        case "4":
+            console.log('chọn số tương ứng với contact để chỉnh Xóa')
+            showListContact();
+            var selectToDelete = readlineSync.question('>>')
+            deleteContact(selectToDelete - 1);
+            saveAll();
+            console.log('Đã xóa!');
+            main()
+            break;
+        default:
+            break;
     }
 }
 main();
