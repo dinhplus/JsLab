dc554f343f566fab2f571e227f9285f5d21bf203
dinhplus <dinhdx.hust@gmail.com>
2020-04-10 15:54:49 +0700

    add HTTPdemo built-in module

---
+++ HttpDemo/demo.js
@@ -0,0 +1,6 @@
+var fs = require("fs");
+var readlineSync = require("readline-sync");
+var path = require('path');
+var co = require("co");
+var requests = require("requests");
+var axios = require("axios");
