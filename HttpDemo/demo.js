a90b0c96c23b236f3e99edc7befaea135752f4b1
dinhplus <dinhdx.hust@gmail.com>
2020-04-13 20:42:17 +0700

    add exercise

--- HttpDemo/demo.js
+++ HttpDemo/demo.js
@@ -1,6 +1,57 @@
 var fs = require("fs");
 var readlineSync = require("readline-sync");
-var path = require('path');
+var path = require("path");
 var co = require("co");
-var requests = require("requests");
+var request = require("request");
 var axios = require("axios");
+
+var userList = JSON.parse(fs.readFileSync("./user.json"));
+// console.log(userList);
+
+function User() {
+	// Thuộc tính
+	this.username = "";
+	this.password = "";
+
+	// Phương thức
+	this.setInfo = function (username, password) {
+		this.username = username;
+		this.password = password;
+	};
+
+	this.checkLogin = function () {
+		let result = false;
+		for (let index = 0; index < userList.length; index++) {
+			if (
+				this.username === userList[index].username &&
+				this.password === userList[index].password
+			)
+				return true;
+		}
+		return result;
+	};
+
+	// Phải return this thì mới tạo mới được đối tượng
+	return this;
+}
+// Cách sử dụng
+
+function login() {
+	var username = readlineSync.question(" what is your user name?");
+	var password = readlineSync.question("passwords ???");
+	var user = new User();
+	user.setInfo(username, password);
+	if (user.checkLogin()) {
+		console.log("Đăng nhập thành công");
+	} else {
+		console.log("Đăng nhập thất bại");
+	}
+}
+login();
+// var user = new User();
+// user.setInfo("admin", "123456");
+// if (user.checkLogin()) {
+// 	console.log("Đăng nhập thành công");
+// } else {
+// 	console.log("Đăng nhập thất bại");
+// }
